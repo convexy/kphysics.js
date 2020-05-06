d5130c6c84997f7f794065350c25d626713fd86d
convexy <shibuya.keigo@gmail.com>
2020-05-06 23:38:14 +0900

    Init.

---
+++ demo/index.js
@@ -0,0 +1,67 @@
+window.addEventListener("load", function () {
+  let body = document.body;
+  let display = window.document.getElementById("main");
+  display.setAttribute("width", 100);
+  display.setAttribute("height", 100);
+  display.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+
+  display.innerHTML =
+    '<rect x="0" y="0" width="100" height="100" fill="#ddd" />';
+
+  let point = {
+    p: new KSphere(3),
+    d: (() => {
+      let d = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+      d.setAttribute("r", 3);
+      d.setAttribute("fill", "orange");
+      display.appendChild(d);
+      return d;
+    })(),
+    synchronize() {
+      this.d.setAttribute("cx", this.p.position.x);
+      this.d.setAttribute("cy", this.p.position.y);
+    },
+  };
+
+  let point2 = {
+    p: new KSphere(3),
+    d: (() => {
+      let d = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+      d.setAttribute("r", 3);
+      d.setAttribute("fill", "orange");
+      display.appendChild(d);
+      return d;
+    })(),
+    synchronize() {
+      this.d.setAttribute("cx", this.p.position.x);
+      this.d.setAttribute("cy", this.p.position.y);
+    },
+  };
+  point.p.position.x = 10;
+  point.p.position.y = 10;
+  point.p.velocity.x = 20;
+  point.p.velocity.y = 20;
+  point2.p.position.x = 90;
+  point2.p.position.y = 10;
+  point2.p.velocity.x = -20;
+  point2.p.velocity.y = 20;
+  point.synchronize();
+  point2.synchronize();
+
+  let last = performance.now();
+
+  requestAnimationFrame(tick);
+
+  function tick(now) {
+    requestAnimationFrame(tick);
+    let dt = now - last;
+    last = now;
+    point.p.update(dt / 1000);
+    point2.p.update(dt / 1000);
+    if (DetectCollision(point.p, point2.p)) {
+      Collision(point.p, point2.p);
+    }
+    point.synchronize();
+    point2.synchronize();
+  }
+});
